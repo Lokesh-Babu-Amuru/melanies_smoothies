526011844b6d59cdc8c9d0b05d3dab891349fb24
Lokesh-Babu-Amuru <amuru.lokesh8@gmail.com>
2024-05-21 15:38:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,32 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col
+from snowflake.snowpark.functions import col, when_matched
 
 # Write directly to the app
-st.title(":cup_with_straw: Customize your smoothie :cup_with_straw:")
+st.title(":cup_with_straw: Pending smoothie orders:cup_with_straw:")
 st.write(
-    """Choose the fruits you want in your custom smoothie.
+    """Orders that need to be filled.
     """
 )
 
-name_on_order = st.text_input("Name on Smoothie")
-st.write("The name on your smoothie will be:", name_on_order)
-
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")=='false').collect()
+if my_dataframe:
+    editable_df = st.data_editor(my_dataframe)
 
-ingredients_list = st.multiselect(
-    "choose upto 5 ingredients:",
-    my_dataframe,
-    max_selections = 5
-)
-
-if ingredients_list:
- ingredients_string = ''
+    submitted = st.button('Submit') 
+    if submitted:
     
- for fruit_chosen in ingredients_list:
-     ingredients_string += fruit_chosen + ' '
-
- #st.write(ingredients_string)
-
- my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
-
- #st.write(my_insert_stmt)
- #st.stop
-    
- time_to_insert = st.button('Submit Orders')   
-
- if time_to_insert:
-      session.sql(my_insert_stmt).collect()
- st.success('Your Smoothie is ordered! '+ name_on_order, icon="✅")
+        og_dataset = session.table("smoothies.public.orders")
+        edited_dataset = session.create_dataframe(editable_df)
+    try:    
+        og_dataset.merge(edited_dataset
+                     , (og_dataset['order_uid'] == edited_dataset['order_uid'])
+                     , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                    )
+        st.success('Someone clicked the button ', icon="👍")
+    except:
+        st.write('Something went wrong.')
+else:
+    st.success('There are no pending orders right now ', icon="👍")   
