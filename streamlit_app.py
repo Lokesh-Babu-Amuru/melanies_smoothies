20a87c660df0d6d9287888a2df9ace3c8112bb79
Lokesh-Babu-Amuru <amuru.lokesh8@gmail.com>
2024-05-21 16:00:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,8 @@ if my_dataframe:
 else:
     st.success('There are no pending orders right now ', icon="👍")   
 
+#import fruityvice information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response.json())
+fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width = True)
